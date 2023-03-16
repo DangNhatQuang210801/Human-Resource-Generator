657e619d21b655134b0204974018e3c18824aaea
Administrator <Administrator@VH44150492>
2023-03-16 12:01:07 +0700

    Add project files.

---
+++ Controllers/GeneratorController.cs
@@ -0,0 +1,19 @@
+﻿using Human_Resource_Generator.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Human_Resource_Generator.Controllers
+{
+    public class GeneratorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public GeneratorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Human_Resource_Generator.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Human_Resource_Generator.Data
+
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee_Training> employee_Training { get; set; }
+    }
+}
