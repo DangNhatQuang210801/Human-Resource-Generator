ae134cb6f28140834888f088d10ef82cecf4303c
Administrator <nhatquang.dang210801@gmail.com>
2023-03-16 13:51:35 +0700

    New models

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,6 +9,19 @@ namespace Human_Resource_Generator.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-        public DbSet<Employee_Training> employee_Training { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee_Training>()
+                .HasOne(b => b.employee)
+                .WithMany(ba => ba.employees_Training)
+                .HasForeignKey(b => b.employee_id);
+            modelBuilder.Entity<Employee_Training>()
+                .HasOne(b => b.training_Program)
+                .WithMany(ba => ba.employees_Training)
+                .HasForeignKey(b => b.program_id);
+        }
+        public DbSet<Employee> employee { get; set; }
+        public DbSet<Training_program> training_Program { get; set; }
+        public DbSet<Employee_Training> employees_Training { get; set; }
     }
 }
