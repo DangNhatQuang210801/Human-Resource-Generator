f142af16faf77f54d8fc958d1134337e8ee6f040
TuanPointC <tuan.nv19010040@st.phenikaa-uni.edu.vn>
2023-04-04 14:38:25 +0700

    ok

---
+++ Migrations/20230401080031_InitialCreate.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Human_Resource_Generator.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrainingPrograms",
+                columns: table => new
+                {
+                    program_id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    program_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    program_description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    date_of_program = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrainingPrograms", x => x.program_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeTrainings",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProgramId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeTrainings", x => new { x.EmployeeId, x.ProgramId });
+                    table.ForeignKey(
+                        name: "FK_EmployeeTrainings_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_EmployeeTrainings_TrainingPrograms_ProgramId",
+                        column: x => x.ProgramId,
+                        principalTable: "TrainingPrograms",
+                        principalColumn: "program_id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeTrainings_ProgramId",
+                table: "EmployeeTrainings",
+                column: "ProgramId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeTrainings");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "TrainingPrograms");
+        }
+    }
+}
